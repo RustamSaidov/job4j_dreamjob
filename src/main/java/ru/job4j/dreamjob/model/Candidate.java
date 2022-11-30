4302878442a8f97a14422e308e7e19fbbf022e11
rusta <rustam_ussr@mail.ru>
2022-11-30 15:40:16 +0300

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,67 @@
+package ru.job4j.dreamjob.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Candidate {
+    private int id;
+    private String name;
+    private String desc;
+    private Date date;
+
+    public Candidate(int id, String name, String desc, Date date) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Candidate)) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return getId() == candidate.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
