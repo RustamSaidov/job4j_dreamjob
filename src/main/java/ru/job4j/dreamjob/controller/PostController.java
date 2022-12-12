b2c8a56e0cb44c7f35ed1aa4021b3521cf0b619d
rusta <rustam_ussr@mail.ru>
2022-12-12 09:39:29 +0300

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.service.PostService;
-import ru.job4j.dreamjob.store.PostStore;
 
 import java.time.LocalDateTime;
 
