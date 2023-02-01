c142a9d3394364c31fe2052553a302ba262fbd9e
rusta <rustam_ussr@mail.ru>
2023-02-01 15:09:06 +0300

    2. HttpSession [#504864]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,13 +1,28 @@
 package ru.job4j.dreamjob.controller;
 
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.model.User;
 
-@RestController
+import javax.servlet.http.HttpSession;
+
+@ThreadSafe
+@Controller
+@RequestMapping("/")
+/*@RestController*/
 public class IndexController {
 
-    @GetMapping("/index")
-    public String getIndex() {
-        return "Hello World!";
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model, HttpSession session) {
+        var user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setName("Гость");
+        }
+        model.addAttribute("user", user);
+        return "/index";
     }
 }
