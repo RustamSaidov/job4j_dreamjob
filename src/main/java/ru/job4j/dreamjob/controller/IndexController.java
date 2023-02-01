eb4b8845c2dc22d2d554920fa151b130089e7257
Qafas <rustam_ussr@mail.ru>
2023-02-01 21:43:16 +0400

    3. Filter [#504865]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -2,12 +2,8 @@ package ru.job4j.dreamjob.controller;
 
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.job4j.dreamjob.model.User;
-
-import javax.servlet.http.HttpSession;
 
 @ThreadSafe
 @Controller
@@ -16,13 +12,7 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     @GetMapping({"/", "/index"})
-    public String getIndex(Model model, HttpSession session) {
-        var user = (User) session.getAttribute("user");
-        if (user == null) {
-            user = new User();
-            user.setName("Гость");
-        }
-        model.addAttribute("user", user);
+    public String getIndex() {
         return "/index";
     }
 }
